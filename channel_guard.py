a5fe8adb5e78da58ae58aeea58fc18231eb9e3a0
RenUs-x <userbotren@gmail.com>
2026-02-22 21:59:59 +0500

    Update channel_guard.py

--- channel_guard.py
+++ channel_guard.py
@@ -133,6 +133,8 @@ async def guard_loop():
 
 
 # ================= MAIN =================
-
+async def main():
+    await channel_guard()
+    
 if __name__ == "__main__":
     asyncio.run(guard_loop())
