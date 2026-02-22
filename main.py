81c588b84322b48b7419aaa03f36342364186526
RenUs-x <userbotren@gmail.com>
2026-02-22 21:53:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import asyncio
 import sss
 import monitor_bot
+import channel_guard
 
 async def main():
     await asyncio.gather(
         sss.main(),
+        channel_guard.main(),
         monitor_bot.main()
     )
 
